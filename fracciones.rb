e82352b836cc7049e3313ae9e54b6a254eaa1159
Juanjo <alu0100615791@ull.edu.es>
2013-10-25 09:44:23 +0100

    Codigo finalizado

--- fracciones.rb
+++ fracciones.rb
@@ -5,12 +5,16 @@ class Fracciones
   attr_accessor :num, :den
   
   def initialize (num, den)
-    @num = num
-    @den = den
+    @num = num/mcd(num, den)
+    @den = den/mcd(num,den)
   end
   
   def mostrar_fraccion
     puts "La fraccion es: #{@num}/#{@den}"
+  end
+  
+  def flotante
+    @num.to_f / @den.to_f
   end
   
   def comparar_igualdad(x)
@@ -48,6 +52,36 @@ class Fracciones
   def resto (x)
     return ((self.num * x.den) % (self.den * x.num))
   end
+
+  def mcd (u, v)
+    u, v = u.abs, v.abs
+    while v != 0
+      u, v = v, u % v
+    end
+    u
+  end
+
+  def mcm (u, v)
+    (u * v)/mcd(u, v)
+  end
+  
+  def suma(x)
+    m = mcm(self.den, x.den)
+    Fracciones.new((m/self.den * self.num) + (m/x.den * x.num),m)
+  end
+  
+  def resta(x)
+    m = mcm(self.den, x.den)
+    Fracciones.new((m/self.den * self.num) - (m/x.den * x.num),m)
+  end
+  
+  def producto(x)
+    Fracciones.new(self.num * x.num, self.den * x.den)
+  end
+  
+  def division(x)
+    Fraccion.new(self.num * x.den, self.den * x.num)
+  end
   
 end
 
@@ -68,6 +102,7 @@ end
   
 #-------------------------------------------------------------------------------------------------------------------------
 
-#num = get_num
-#den = get_den
-#a = Fracciones.new(num, den)
+num = get_num
+den = get_den
+a = Fracciones.new(num, den)
+a.mostrar_fraccion
